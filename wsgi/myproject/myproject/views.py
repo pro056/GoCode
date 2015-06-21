1f8254acf295c2831b6fd4fd64e9a0127d313642
unknown <I317409@INLN50813933A.apj.global.corp.sap>
2015-06-21 16:30:11 +0530

    new commit

--- wsgi/myproject/myproject/views.py
+++ wsgi/myproject/myproject/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import requests
 from django.http import HttpResponse
-
+from django.views.decorators.csrf import csrf_exempt
 from datetime import datetime
 from models import *
 import simplejson
@@ -12,12 +12,14 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+@csrf_exempt
 def createUser (request):
 	json_data = json.loads (request.body)
 	newUser = User(user_id=json_data["userid"], password=json_data["passwd"], first_name=json_data["fname"], last_name=json_data["lname"], email_id=json_data["email"])
 	newUser.save()
 	return JSONResponse (status=201)
 
+@csrf_exempt
 def setHandle (request):
 	json_data = json.loads(request.body)
 	user = User.objects.get (user_id=json_data["userid"])
