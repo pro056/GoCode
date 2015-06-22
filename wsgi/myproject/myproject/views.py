b25bf5da07d86aef7fea61094d4c046a587706f6
unknown <I317409@INLN50813933A.apj.global.corp.sap>
2015-06-22 23:17:52 +0530

    new commit

--- wsgi/myproject/myproject/views.py
+++ wsgi/myproject/myproject/views.py
@@ -13,9 +13,6 @@ from django.http import HttpResponse
 
 
 class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
 	def __init__(self, data, **kwargs):
 		content = JSONRenderer().render(data)
 		kwargs['content_type'] = 'application/json'
